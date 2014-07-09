08c8c9c911fd8671b5f32486e57aaa901756faa6
qlir <parafus@yandex.ru>
2014-07-10 00:19:07 +0400

    creates startes struct

---
+++ Isuct.UniversitySchedule/scripts/ResponseController.js
@@ -0,0 +1,15 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Sergey
+ * Date: 08.07.14
+ * Time: 22:15
+ * To change this template use File | Settings | File Templates.
+ */
+ResponseController = {
+    getGroupsList : function(onsuccess, onerror) {
+    },
+
+    getScheduleByGroup : function(idGroup, onsuccess, onerror) {
+
+    }
+}
---
+++ Isuct.UniversitySchedule/scripts/scheduleController.js
@@ -0,0 +1,10 @@
+function ScheduleController() {
+    this.Schedule;
+
+    this.getScheduleByData = function(data) {};
+
+    this.loadScheduleForGroup = function(groupId) {};
+
+    this.loadLastSchedule = function() {};
+
+}
